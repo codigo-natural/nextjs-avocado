e18e3758c31c9f61136534c9eff458ec20bb88e6
Code-Nature <codenature01001@gmail.com>
2023-04-13 16:13:48 -0500

    documentando el codigo de la API

--- database/db.ts
+++ database/db.ts
@@ -6,24 +6,30 @@ import allData from './data'
 class Database {
   constructor() {}
 
+  // Método asíncrono para obtener todos los productos de la base de datos simulada
   async getAll(): Promise<TProduct[]> {
     const asArray = Object.values(allData)
-    await randomDelay()
+    await randomDelay() // Esperar un tiempo aleatorio para simular el retraso de una consulta de base de datos real
     return asArray
   }
 
+  // Método asíncrono para obtener un producto por ID de la base de datos simulada
   async getById(id: string): Promise<TProduct | null> {
+    // Comprobar si el ID del producto existe en la base de datos simulada
     if (!Object.prototype.hasOwnProperty.call(allData, id)) {
       return null
     }
 
+    // Si el ID del producto existe, devolver la entrada correspondiente de la base de datos
     const entry = allData[id]
-    await randomDelay()
+    await randomDelay() // Esperar un tiempo aleatorio para simular el retraso de una consulta de base de datos real
     return entry
   }
 }
 
 // Let's also add a delay to make it a bit closer to reality
+// Función para simular el retraso de una consulta de base de datos real
+// Se agrega un retraso aleatorio entre 100 y 350 milisegundos antes de resolver la promesa
 const randomDelay = () =>
   new Promise((resolve) => {
     const max = 350
@@ -33,4 +39,4 @@ const randomDelay = () =>
     setTimeout(resolve, delay)
   })
 
-export default Database
+export default Database // Exportar la clase Database como un módulo
--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -2,19 +2,26 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import DB from '@database'
 import enablePublicAccess from '@cors'
 
+// Función asíncrona que maneja la solicitud HTTP GET para obtener detalles de un aguacate en particular y devuelve una respuesta en formato JSON
 const AvoDetail = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // Generally, you would not want this in your apps.
     // See more in 'cors.js'
+
+    // Habilitar el acceso público a la API
     await enablePublicAccess(req, res)
 
+    // Crear una nueva instancia de la clase DB para conectarse a la base de datos
     const db = new DB()
+    // Obtener el ID del aguacate de los parámetros de consulta de la solicitud
     const avoId = req.query.id as string
 
+    // Obtener los detalles del aguacate utilizando el método getById
     const avo = await db.getById(avoId)
 
     // Notice: We're using Next.JS response helpers here :)
     // https://nextjs.org/docs/api-routes/response-helpers
+    // Devolver los detalles del aguacate en formato JSON utilizando la función de ayuda proporcionada por Next.js
     res.status(200).json(avo)
   } catch (e) {
     console.error(e)
@@ -22,4 +29,4 @@ const AvoDetail = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 }
 
-export default AvoDetail
+export default AvoDetail // Exportar la función AvoDetail como un módulo
